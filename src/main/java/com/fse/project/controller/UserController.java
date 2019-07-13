4fb437937bbf860d73b12524441957ce8790aafd
Saravanan P <psaravanan.mailme@gmail.com>
2019-07-13 23:56:44 +0530

    updated

--- src/main/java/com/fse/project/controller/UserController.java
+++ src/main/java/com/fse/project/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,7 +61,7 @@ public class UserController {
 		return userService.getAllUsers();
 	}
 	
-	@GetMapping("/deleteUser/{userId}")
+	@DeleteMapping("/deleteUser/{userId}")
 	public String deleteUser (@PathVariable Long userId){
 		return userService.deleteUser(userId);
 	}
